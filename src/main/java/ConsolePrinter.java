fa3d7cd85cef2540a6684a9c0732fd67244f0d74
a.bezrukov <a.bezrukov@tinkoff.ru>
2022-03-25 12:47:38 +0300

    docs: added javadoc comments to ConsolePrinter.java

--- src/main/java/ConsolePrinter.java
+++ src/main/java/ConsolePrinter.java
@@ -1,4 +1,13 @@
+/**
+ * The {@code ConsolePrinter} class contains single method for console output of passed value
+ * @author Eugene Krivosheyev
+ */
 public class ConsolePrinter {
+    /**
+     * Prints the passed string to the console.
+     *
+     * @param message string to be printed in console.
+     */
     public static void print(String message) {
         System.out.println(message);
     }
