45113051abbb369817bd35f1f6df1f9852889046
a.bezrukov <a.bezrukov@tinkoff.ru>
2022-04-15 15:21:14 +0300

    feat: added opportunity to decorate few message strings in one MessageService.process call.
    docs: updated javadoc comments

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -1,15 +1,11 @@
 package com.tcs.edu;
 
-import com.tcs.edu.decorator.Severity;
 import com.tcs.edu.service.MessageService;
+import static com.tcs.edu.decorator.Severity.REGULAR;
 
 class Application {
     public static void main(String[] args) {
-        MessageService.process("Hello, world!", Severity.MINOR);
-        MessageService.process("Hello, world!", Severity.REGULAR);
-        MessageService.process("Hello, world!", Severity.MAJOR);
-        MessageService.process("Hello, world!", Severity.MAJOR);
-        MessageService.process("Hello, world!", Severity.REGULAR);
-        MessageService.process("Hello, world!", Severity.MINOR);
+        MessageService.process(REGULAR, "Hello, world!0", "Hello, world!1", "Hello, world!2",
+                "Hello, world!3", "Hello, world!4", "Hello, world!5");
     }
 }
--- src/main/java/com/tcs/edu/service/MessageService.java
+++ src/main/java/com/tcs/edu/service/MessageService.java
@@ -1,14 +1,10 @@
 package com.tcs.edu.service;
 
-import com.tcs.edu.decorator.PaginationDecorator;
-import com.tcs.edu.decorator.TimestampMessageDecorator;
-import com.tcs.edu.printer.ConsolePrinter;
 import com.tcs.edu.decorator.Severity;
-
+import com.tcs.edu.printer.ConsolePrinter;
 import static com.tcs.edu.decorator.PaginationDecorator.paginate;
 import static com.tcs.edu.decorator.SeverityDecorator.severityMapToString;
 import static com.tcs.edu.decorator.TimestampMessageDecorator.decorate;
-import static com.tcs.edu.printer.ConsolePrinter.print;
 
 /**
  * The {@code MessageService} class contains a method for composing and printing of some message sting.
@@ -23,9 +19,17 @@ public class MessageService {
      * Printed rows are collected in pages of PaginationDecoration.PAGE_SIZE, pages  separated by "---" separator.
      *
      * @param message string to be decorated with the current timestamp, severity level, message counter.
+     * @param messages varargs of strings to be decorated with the current timestamp, severity level, message counter.
      * @param level one of severity enum levels.
      */
-    public static void process(String message, Severity level){
-        print(paginate(decorate(message) + " " + severityMapToString(level)));
+    public static void process( Severity level, String message, String... messages){
+
+        ConsolePrinter.print(
+                paginate(decorate(message) + " " + severityMapToString(level)));
+
+        for (String mess: messages){
+            ConsolePrinter.print(
+                    paginate(decorate(mess) + " " + severityMapToString(level)));
+        }
     }
 }
