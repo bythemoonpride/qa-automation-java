2c8e2ceccc08038450ba63e336e1dc5c503bda7f
a.bezrukov <a.bezrukov@tinkoff.ru>
2022-05-20 17:15:32 +0300

    feat: added ProcessException, that thrown to indicate a message processing has failed, added test methods in Application refactor: refactored private methods in OrderedDistinctedMessageService docs: added and updated javadoc comment

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -3,10 +3,11 @@ package com.tcs.edu;
 import com.tcs.edu.decorator.TimestampPaginationDecorator;
 import com.tcs.edu.domain.Message;
 import com.tcs.edu.printer.ConsolePrinter;
+import com.tcs.edu.project_enum.Doubling;
+import com.tcs.edu.project_enum.MessageOrder;
 import com.tcs.edu.service.MessageService;
 import com.tcs.edu.service.OrderedDistinctedMessageService;
-
-import java.sql.SQLOutput;
+import com.tcs.edu.service.ProcessException;
 
 import static com.tcs.edu.project_enum.Doubling.*;
 import static com.tcs.edu.project_enum.MessageOrder.*;
@@ -56,5 +57,49 @@ class Application {
 
         System.out.println("\n");
         System.out.println("Хэшкод сообщения: " + message1.hashCode());
+        System.out.println("\n");
+
+        checkMessageServiceExceptions(ProcessException.class, null, DOUBLES);
+        checkMessageServiceExceptions(ProcessException.class, ASC, null);
+        checkMessageServiceExceptions(ProcessException.class, DESC, DISTINCT);
+        checkMessageServiceExceptions(IllegalArgumentException.class, ASC, null);
+
+    }
+
+    public static <T extends Exception> void checkMessageServiceExceptions(Class<T> expected,
+                                                                           MessageOrder order, Doubling doubling) {
+        MessageService messageService = new OrderedDistinctedMessageService(
+                new TimestampPaginationDecorator(),
+                new ConsolePrinter()
+        );
+        exceptionAssert(expected, () -> messageService.process(order, doubling, new Message(REGULAR, "Hello!")));
+    }
+
+    private static <T extends Exception> T exceptionAssert(Class<T> expected, Runnable runnable) {
+        T exception = null;
+        boolean isExceptionGetCaught = false;
+        try {
+            runnable.run();
+        } catch (Exception e) {
+            if (expected.isInstance(e)) {
+                System.out.println(
+                        String.format("Проверка успешно пройдена, ожидалось исключение %s, получили исключение %s.",
+                                expected.getSimpleName(), e.getClass().getSimpleName()));
+                exception = (T) e;
+            } else {
+                System.out.println(
+                        String.format("Проверка завершилась неудачей, " +
+                                        "ожидалось исключение %s, но получили исключение %s.",
+                                expected.getSimpleName(), e.getClass().getSimpleName()));
+            }
+            isExceptionGetCaught = true;
+        }
+
+        if (!isExceptionGetCaught) {
+            System.out.println(
+                    String.format("Проверка завершилась неудачей, ожидалось исключение %s, но ничего не получили.",
+                    expected.getSimpleName()));
+        }
+        return exception;
     }
 }
